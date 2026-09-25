e02149251edad6965a239298f12bf1d7fd626d46
clientcarex-cloud <clientcarex@gmail.com>
2026-09-25 11:24:55 +0530

    Remove Client Login link from topbar for streamlined navigation

--- views/partials/topbar.php
+++ views/partials/topbar.php
@@ -7,7 +7,6 @@
     <ul class="topbar__contact">
       <li><a href="tel:<?= PHONE_HREF ?>"><?= icon('phone') ?>Call: <?= PHONE ?></a></li>
       <li><a href="mailto:<?= EMAIL ?>"><?= icon('mail') ?><?= EMAIL ?></a></li>
-      <li><a href="<?= APP_LOGIN ?>"><?= icon('arrow') ?>Client Login</a></li>
     </ul>
   </div>
 </div>
